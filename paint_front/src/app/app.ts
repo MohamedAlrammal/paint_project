ed1b6cfadafaa2bffce57f8ffc5a038697e53125
ahmdkaml <ahmdkaml2006@gmail.com>
2025-11-14 13:10:29 +0200

    tool-bar

--- paint_front/src/app/app.ts
+++ paint_front/src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolBar } from './tool-bar/tool-bar';
+import { DrawTools } from './types/enum';
+import { Test } from "./test/test";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolBar, Test],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  tool: DrawTools = DrawTools.Mouse;
+  onToolChange(selectedTool: DrawTools) {
+    this.tool = selectedTool;
+  }
   protected readonly title = signal('paint_front');
 }
